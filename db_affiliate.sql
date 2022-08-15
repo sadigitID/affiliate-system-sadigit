2f19a939f2ea2ec3fbf6a540a2bfd70ddedcf6e1
Salma Azizah <90431994+salrnazizah@users.noreply.github.com>
2022-08-15 14:35:34 +0700

    Menambah database yang telah direvisi
    menambahkan nama_produk beserta harga_jual pada tb_pesanans

--- db_affiliate.sql
+++ db_affiliate.sql
@@ -45,6 +45,8 @@ CREATE TABLE `tb_pesanans` (
   `id_pesanan` int(11) NOT NULL,
   `id_produk` int(11) DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL,
+  `nama_produk` varchar(100) DEFAULT NULL,
+  'harga_jual' double DEFAULT NULL,
   `nama_pembeli` varchar(50) DEFAULT NULL,
   `tanggal_pembayaran` date DEFAULT NULL,
   `tanggal_pesanan` date DEFAULT NULL,
